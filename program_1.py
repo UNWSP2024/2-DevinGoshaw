6946d7e3c66b1dfaf40f4c5bcfe7b7c2d7ab6d50
DevinGoshaw <DRGoshaw@students.unwsp.edu>
2025-09-08 10:06:19 -0500

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,6 +1,7 @@
 def personal_information():
-    print('...')
-
-
+    print('Name: Devin Goshaw')
+    print('Adress: 11914 Malaki Lark N, Frangle, China')
+    print("Phone Number: 666-983-2938")
+    print('College Major: undecided')
 # Line which calls the above function.
 personal_information()
