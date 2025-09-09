1dd05c1307fd41c815a975f340e19e91f9af4027
DevinGoshaw <DRGoshaw@students.unwsp.edu>
2025-09-09 09:00:57 -0500

    Update wk2_program_1.py

--- wk2_program_1.py
+++ wk2_program_1.py
@@ -1,11 +1,11 @@
 #fake info program
 #Programmer: Devin Goshaw   
-#Date 9/8/2025
+#Date 9/9/2025
 def personal_information():
     print('Name: Devin Goshaw')
     print('Adress: 11914 Malaki Lark N, Frangle, MN 55555')
     print("Phone Number: 666-983-2938")
     print('College Major: undecided')
 
-# Line which calls the above function.
+# Line which calls the above function. 
 personal_information()
